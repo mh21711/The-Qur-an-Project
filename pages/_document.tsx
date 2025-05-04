520481a5c96b4a7a305ed31ff849237b1b153b51
mh21711 <elrashedymahmoud217@gmail.com>
2025-05-04 11:25:21 +0300

    Adding Some Features

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="hfDvv08MMEnEtP-vqauPIKPZ_5_UvBTkjM-IaV1XGAo"
+        />
+        <title>Quran</title>
+        <meta
+          name="description"
+          content="This a website where you can read or listen to quran and the explain of the ayah and there is a lot of readers you can listen to."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
