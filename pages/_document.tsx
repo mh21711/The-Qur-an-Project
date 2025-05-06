1a2515f11cbcb355242cc1515a4f9c3d39a6288a
mh21711 <elrashedymahmoud217@gmail.com>
2025-05-06 12:52:56 +0300

    Trying To Make My Website Appear In google Search

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,11 +8,17 @@ export default function Document() {
           name="google-site-verification"
           content="5Fn3rPvNO4oGrqunk4VWGakt-OyxNzesnMndHzC6B8k"
         />
-        <title>Quran 217 – Listen and Read the Quran</title>
+        <meta property="og:site_name" content="Quran_217.com" />
         <meta
-          name="description"
+          property="og:title"
+          content="Quran 217 – Listen and Read the Quran"
+        />
+        <meta
+          property="og:description"
           content="Quran 217 is a website where you can read or listen to the Quran, explore Ayah explanations, and choose from many reciters."
         />
+        <meta property="og:url" content="https://quran-217.netlify.app" />
+        <meta property="og:image" content="https://quran-217.netlify.app/images/Website_Image.png" />
       </Head>
       <body>
         <Main />
